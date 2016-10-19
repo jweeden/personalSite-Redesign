7992db6f026ee55b00b9905221f86f96831142d3
jweeden <joseph.p.weeden@gmail.com>
2016-10-19 16:21:43 -0700

    Full screen gallery options
    mostly done, gonna explore other options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-$(document).ready(function(){
-
-  $("button.one").click(function(){
-    $("div.one").toggle(500);
-  });
-
-  $("button.two").click(function(){
-    $("div.two").toggle(500);
-  });
-
-  $("button.three").click(function(){
-    $("div.three").toggle(500);
-  });
+$(function() {
 
 });
